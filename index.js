06d286053741786ee6e60ea6328ca352c102c3af
frankos6 <frankos6@wp.pl>
2023-10-10 22:35:35 +0200

    auth middleware, user and device model update

--- index.js
+++ index.js
@@ -14,26 +14,6 @@ try {
     console.error('Unable to connect to the database:', error);
 }
 
-const Device = sequelize.define('Device',{
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    isConnected: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-    },
-    lastSeen: DataTypes.DATE
-},{
-    tableName: 'IoTDevices'
-})
-
 const User = sequelize.define('User', {
     id: {
         type: DataTypes.INTEGER,
@@ -53,8 +33,40 @@ const User = sequelize.define('User', {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
+    },
+    isDevice: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
     }
 })
+
+const Device = sequelize.define('Device',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isConnected: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    lastSeen: DataTypes.DATE,
+},{
+    tableName: 'IoTDevices',
+})
+
+Device.belongsTo(User);
+User.hasOne(Device,{
+    foreignKey: {
+        allowNull: false
+    }
+});
 
 const DeviceData = sequelize.define('DeviceData',{
     id: {
@@ -130,14 +142,47 @@ await sequelize.sync({alter: true})
 
 app.use((req, res, next) => {
     res.on('finish', async () => {
-        console.log(`endpoint ${req.path} - response code ${res.statusCode} ${res.body | ''}`)
+        console.log(`endpoint ${req.path} - response code ${res.statusCode} ${res.body || ''}`)
         await APILog.create({endpoint:req.path, responseStatus:res.statusCode})
     })
     next()
 })
-
+// endpoints without auth
 app.get('/',(req,res)=>{
     res.send('hello world!')
 });
+
+app.use(async (req, res, next)=>{
+    if (!req.headers.authorization) {
+        res.status(401).send()
+        return
+    }
+    const token = req.headers.authorization.split(' ')[1]
+    if (!token || token === '') {
+        res.status(401).send("No credentials")
+        return
+    }
+    const [username, password] = atob(token).split(':');
+    const users = await User.findAll({
+        where: {
+            username
+        }
+    })
+    if (users.length !== 1) {
+        res.status(401).send("Invalid credentials")
+        return
+    }
+    if (users[0].password !== password) {
+        res.status(401).send("Invalid password")
+        return
+    }
+    next()
+})
+
+// endpoints with auth
+
+app.get('/auth',(req,res)=>{
+    return res.status(200).send("Your credentials are correct!")
+})
 
 app.listen(245,()=>console.log(`listening on port 245`))

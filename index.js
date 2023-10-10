a0a13c03149c8e5cd547f67babff798f9060a45a
frankos6 <frankos6@wp.pl>
2023-10-10 19:49:58 +0200

    .gitignore, database, models and api logging

--- index.js
+++ index.js
@@ -1,8 +1,143 @@
 import express from "express";
+import {Sequelize, DataTypes} from "sequelize";
 
 const app = express();
+const sequelize = new Sequelize({
+    dialect:'sqlite',
+    storage:'database.db'
+})
 
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+const Device = sequelize.define('Device',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isConnected: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    lastSeen: DataTypes.DATE
+},{
+    tableName: 'IoTDevices'
+})
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+})
+
+const DeviceData = sequelize.define('DeviceData',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    payload: DataTypes.JSON
+},{
+    timestamps: true,
+    updatedAt: false,
+    createdAt: 'timestamp'
+})
+
+Device.hasMany(DeviceData)
+DeviceData.belongsTo(Device)
+
+const DeviceConfiguration = sequelize.define('DeviceConfiguration',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    deviceId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    params: DataTypes.JSON
+})
+
+Device.hasMany(DeviceConfiguration,{
+    foreignKey: 'deviceId'
+})
+DeviceData.belongsTo(Device)
+
+const Alert = sequelize.define('Alert',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    message: DataTypes.STRING
+},{
+    timestamps: true,
+    updatedAt: false,
+    createdAt: 'timestamp'
+})
+
+Device.hasMany(Alert)
+Alert.belongsTo(Device)
+
+const APILog = sequelize.define('APILog',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    endpoint: DataTypes.STRING,
+    responseStatus: DataTypes.INTEGER
+},{
+    timestamps: true,
+    updatedAt: false,
+    createdAt: 'timestamp',
+    tableName: 'APIRequestLog'
+})
+
+
+await sequelize.sync({alter: true})
 
 app.use((req, res, next) => {
-    //api request logging
+    res.on('finish', async () => {
+        console.log(`endpoint ${req.path} - response code ${res.statusCode} ${res.body | ''}`)
+        await APILog.create({endpoint:req.path, responseStatus:res.statusCode})
+    })
+    next()
 })
+
+app.get('/',(req,res)=>{
+    res.send('hello world!')
+});
+
+app.listen(245,()=>console.log(`listening on port 245`))

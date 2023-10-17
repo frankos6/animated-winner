1eaa48aa79aa4a77007500cc8fbf76bc269ff296
frankos6 <frankos6@wp.pl>
2023-10-17 22:14:34 +0200

    get device config, get all alerts, delete device, limit endpoints to users

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.pre(restify.pre.sanitizePath())
 app.use(restify.plugins.bodyParser({rejectUnknown: true}))
 app.use(restify.plugins.throttle({
     burst: 10,
-    rate: 2,
+    rate: 10,
     ip: true,
     overrides: {
         '127.0.0.1': {
@@ -17,8 +17,8 @@ app.use(restify.plugins.throttle({
 }))
 app.use(restify.plugins.authorizationParser())
 const sequelize = new Sequelize({
-    dialect:'sqlite',
-    storage:'database.db'
+    dialect: 'sqlite',
+    storage: 'database.db'
 })
 
 try {
@@ -154,7 +154,7 @@ const APILog = sequelize.define('APILog',{
 })
 
 
-await sequelize.sync({alter: true})
+await sequelize.sync()
 
 app.on('after',async (req,res,route)=>{
     console.log(`endpoint ${route ? route.path : req.getPath()} - response code ${res.statusCode} ${res.body || ''}`)
@@ -166,11 +166,13 @@ app.get('/',async (req,res)=>{
 });
 
 app.post('/device/register', async (req, res)=>{
-    // TODO: use transaction
     const devices = await Device.count()
-    const newUser = await User.create({username: `device${devices}`, password: Math.random().toString(36).substring(2,10), isDevice: true})
-    await Device.create({name: newUser.username, UserId: newUser.id})
-    return res.send(201,{username: newUser.username, password: newUser.password})
+    const {username, password} = await sequelize.transaction(async (t) => {
+        const newUser = await User.create({username: `device${devices+1}`, password: Math.random().toString(36).substring(2,10), isDevice: true}, {transaction: t})
+        await Device.create({name: newUser.username, UserId: newUser.id}, {transaction: t})
+        return newUser
+    })
+    return res.send(201,{username, password})
 })
 
 app.post('/user/register', async (req, res)=>{
@@ -212,36 +214,89 @@ const authFn = async (req, res)=>{
     req.user = users[0];
 }
 
+// device id param handler
+const deviceFn = async (req, res) => {
+    if (!req.params.id || req.params.id === '') throw new errors.MissingParameterError('You need to provide a device id')
+    const device = await Device.findOne({ where: { id: req.params.id } })
+    if (!device) throw new errors.NotFoundError(`A device with id ${req.params.id} does not exist`)
+    req.device = device
+}
+
+// permission handler
+const adminFn = async (req, res) => {
+    if (!req.user.isAdmin) throw new errors.UnauthorizedError('Only an admin can perform this action')
+}
+
+// handler for user-only routes
+const isUserFn = async (req, res) => {
+    if (req.user.isDevice) throw new errors.UnauthorizedError('Devices cannot access this endpoint')
+}
+
+// handler for device-only routes
+const isDeviceFn = async (req, res) => {
+    if (!req.user.isDevice) throw new errors.UnauthorizedError('Only devices can access this endpoint')
+    req.device = await Device.findOne({where: {UserId: req.user.id}})
+}
+
 // endpoints with auth
 
-app.get('/auth',[authFn, async (req, res)=>{
+app.get('/auth',[authFn, async (req, res) => {
     return res.send(`Your credentials are correct!\nYou are logged in as ${req.username}.`)
 }])
 
-app.get('/device/list',[authFn, async (req,res)=>{
+app.get('/device/list',[authFn, isUserFn, async (req,res) => {
     const devices = await Device.findAll();
     return res.send(devices.map(d=>d.toJSON()))
 }])
 
-app.get('/device/:id',[authFn, async (req, res)=>{
-    if (!req.params.id || req.params.id === '') throw new errors.MissingParameterError('You need to provide a device id')
-    const device = await Device.findOne({ where: { id: req.params.id } })
-    if (!device) throw new errors.NotFoundError(`A device with id ${req.params.id} does not exist`)
-    else return res.send(device.toJSON())
+app.get('/device/:id',[authFn, isUserFn, deviceFn,
+    async (req, res) => res.send(req.device.toJSON())
+])
+
+app.del('/device/:id',[authFn, isUserFn, deviceFn, adminFn, async (req, res) => {
+    await sequelize.transaction(async (transaction) => {
+        await DeviceConfiguration.destroy({where: {deviceId: req.device.id}, transaction})
+        await Alert.destroy({where: {deviceId: req.device.id}, transaction})
+        await DeviceData.destroy({where: {deviceId: req.device.id}, transaction})
+        await Device.destroy({where: {id: req.device.id}, transaction})
+        await User.destroy({where: {id: req.device.UserId}, transaction})
+    })
+    res.send(204)
 }])
 
-app.get('/device/:id/alerts',[authFn, async (req, res) => {
-    if (!req.params.id || req.params.id === '') throw new errors.MissingParameterError('You need to provide a device id')
-    const device = await Device.findOne({ where: { id: req.params.id } })
-    if (!device) throw new errors.NotFoundError(`A device with id ${req.params.id} does not exist`)
-    return res.send((await Alert.findAll({where: { id: req.params.id }})).map(e=>e.toJSON()))
+app.get('/device/:id/alerts',[authFn, isUserFn, deviceFn,
+    async (req, res) => res.send((await Alert.findAll({where: {deviceId: req.params.id }})).map(e=>e.toJSON()))
+])
+
+app.get('/device/:id/data',[authFn, isUserFn, deviceFn,
+    async (req, res) => res.send((await DeviceData.findAll({where: {deviceId: req.params.id}, limit: req.query.limit || 10})).map(e=>e.toJSON()))
+])
+
+app.get('/device/:id/config',[authFn, isUserFn, deviceFn, adminFn,
+    async (req, res)=> {
+        const config = await DeviceConfiguration.findOne({where: {deviceId: req.params.id}})
+        if (!config) res.send({})
+        else res.send(config.toJSON())
+    }
+])
+
+app.put('/device/:id/config',[authFn, isUserFn, deviceFn, adminFn,
+    async (req, res)=>{
+    if (await DeviceConfiguration.findOne({where: {deviceId: req.params.id}})) {
+        await DeviceConfiguration.update({params: req.body},{
+            where: {
+                deviceId: req.params.id
+            }
+        })
+        res.send(200)
+    } else {
+        await DeviceConfiguration.create({deviceId: req.params.id, params: req.body})
+        res.send(201)
+    }
 }])
 
-app.get('/device/:id/data',[authFn, async (req, res)=>{
-    if (!req.params.id || req.params.id === '') throw new errors.MissingParameterError('You need to provide a device id')
-    const device = await Device.findOne({ where: { id: req.params.id } })
-    if (!device) throw new errors.NotFoundError(`A device with id ${req.params.id} does not exist`)
-    return res.send((await DeviceData.findAll({where: {id: req.params.id}, limit: req.query.limit || 10})).map(e=>e.toJSON()))
-}])
+app.get('/alerts', [authFn, isUserFn,
+    async (req, res)=> res.send((await Alert.findAll({limit: req.query.limit || 10})).map(e=>e.toJSON()))
+])
 
 app.listen(245,()=>console.log(`listening on port 245`))

e2558421ecba1f1372363fc232a9384bf0180111
Piotr Czupajło <107940568+stasiososna@users.noreply.github.com>
2023-10-20 10:55:41 +0200

    getting username and password from random api

--- esp8266.ino
+++ esp8266.ino
@@ -5,23 +5,27 @@
 #include <Adafruit_Sensor.h>
 #include <FS.h>
 #include <Preferences.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define DHTPIN 5
 #define DHTTYPE DHT11
 
 ESP8266WebServer server;
+WiFiClient client;
 int pin_led = D4;
 String ssid = "desktop1";
-String password = "12345678";
+String passwordd = "12345678";
 DHT dht(DHTPIN,DHTTYPE);
 float t=0.0;
 float h = 0.0;
+String username;
+String password;
 
 Preferences preferences;
 
 void setup() {
-  
-  
+  preferences.begin("esp");
   pinMode(pin_led,HIGH);
   Serial.begin(9600);
 
@@ -30,7 +34,7 @@ void setup() {
   delay(10);
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, passwordd);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -42,15 +46,17 @@ void setup() {
   if(MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
+setAcces();
 server.on("/data", []() {
   Serial.println("Serving /data");
   server.sendHeader("Access-Control-Allow-Origin", "*");
-  String username = preferences.getString("username","User");
-  String password = preferences.getString("password","User");
+  username = preferences.getString("username","User");
+  password = preferences.getString("password","User");
+  
   String humstring = String(h);
   String tempstring=String(t);
   
-  String json = "{\"Temperature\":\""  + tempstring + "\",\"Humidity\":\""+ h +"\", \" Username\":\""+username+"\",\"Password\":\""+password+"\"}";
+  String json = "{\"Temperature\":\""  + tempstring + "\",\"Humidity\":\""+ h +"\", \"Username\":\""+username+"\",\"Password\":\""+password+"\"}";
   server.send(200, "application/json",json);
 });
   server.on("/toggle",toggleLED);
@@ -66,11 +72,47 @@ void loop() {
   server.handleClient();
   MDNS.update();
 }
+void setAcces()
+{
+   username = preferences.getString("username","User");
+   password = preferences.getString("password","User");
+  Serial.println("====");
+  Serial.println(username);
+   if(username=="User")
+  {
+    Serial.println("1");
+  HTTPClient http; //Object of class HTTPClient
+    http.begin(client,"http://jsonplaceholder.typicode.com/users/1");
+    int httpCode = http.GET();
 
+    if (httpCode > 0) 
+    {
+
+  
+      DynamicJsonDocument jsonBuffer(1024);
+      deserializeJson(jsonBuffer,http.getString());
+      Serial.println(http.getString());
+      JsonObject root = jsonBuffer.as<JsonObject>();
+       username = String(root["name"]); 
+       password = String(root["username"]); 
+    
+    Serial.println(preferences.getString("password","User"));
+    Serial.println("haslo");
+    Serial.println(username);
+    }
+    
+    http.end(); //Close connection
+
+  }
+  preferences.putString("username",username);
+  preferences.putString("password",password);
+  Serial.println("||||||");
+  Serial.println(preferences.getString("username","User"));
+  }
 void toggleLED()
 {
   digitalWrite(pin_led,!digitalRead(pin_led));
-  preferences.putString("username","User1");
-  preferences.putString("password","User2");
+  setAcces();
+  
   server.send(204,"");
 }

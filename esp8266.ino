fa8d3e0be8420485bc82ba57d6b6a39bcd156d84
Piotr Czupajło <107940568+stasiososna@users.noreply.github.com>
2023-10-31 09:07:21 +0100

    Finish update / change the servname

--- esp8266.ino
+++ esp8266.ino
@@ -9,6 +9,9 @@
 #include <ArduinoJson.h>
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
+#include <base64.h>
+#include <iostream>
+#include <string>
 
 #define DHTPIN 5
 #define DHTTYPE DHT11
@@ -23,7 +26,7 @@ float t=0.0;
 float h = 0.0;
 String username;
 String password;
-
+using namespace std;
 
 Preferences preferences;
 
@@ -49,65 +52,141 @@ void setup() {
   if(MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
-  setAcces();
-server.on("/data", []() {
-  Serial.println("Serving /data");
-  server.sendHeader("Access-Control-Allow-Origin", "*");
   username = preferences.getString("username","User");
   password = preferences.getString("password","User");
+  String configuration = preferences.getString("minTemp","0");
+  Serial.println("zzz");
+  Serial.println(configuration);
+  if(configuration=="0")
+  {
+    getconfiguration();
+    
+  }
+  if(username=="User")
+  {
+    
+    setAcces();
+  }
   
-  String humstring = String(h);
-  String tempstring = String(t);
-  
-  String json = "{\"Temperature\":\""  + tempstring + "\",\"Humidity\":\""+ h +"\", \"Username\":\""+username+"\",\"Password\":\""+password+"\"}";
-  server.send(200, "application/json",json);
-});
   server.on("/toggle",toggleLED);
-  preferences.begin("esp82");
+  server.on("/sim",sim);
+  server.on("/clear", clearr);
   
   server.begin();
   Serial.println("HTTP server started");
 }
 
+
 void loop() { 
+  int over = 0;
+  String alertmessage;
   HTTPClient http;
   delay(10000);
+    t=dht.readTemperature();
+  h=dht.readHumidity();
   String humstring = String(h);
   String tempstring = String(t);
-  http.begin(client, serverName);
-  //change server name !!!
+  String servnamedata = serverName+"/data";
+  http.begin(client, servnamedata);
+
   username = preferences.getString("username","User");
   password = preferences.getString("password","User");
-  Serial.println(username);
-  String postvalue = "username=" +username+"&password =" +password+"&t="+t+"&h="+h;
+
+  String postvalue = username+":" +password;
+  int overheat = preferences.getInt("maxTemp",100);
+  int overcooling = preferences.getInt("minTemp",1);
+  int overhum1 = preferences.getInt("maxHumidity",100);
+  int overhum2 = preferences.getInt("minHumidity",1);
+
+
+  String base64code = base64::encode(postvalue);
+
+  Serial.println("DATA");
+  Serial.println(t);
+  Serial.println(h);
+  Serial.println(overheat);
+  Serial.println(overcooling);
+  Serial.println(overhum1);
+  Serial.println(overhum2);
+  Serial.println(over);
+
+  
+  if(t>overheat  || t<overcooling || h>overhum1 || h<overhum2)
+  {
+    over= 1;  
+    if(t>overheat || t<overcooling)
+    {
+      if(t>overheat)
+      {
+        alertmessage = "Temperature to High";
+      }
+      else{
+        alertmessage = "Temperature to Low" ;
+        }
+    }
+    else{
+          if(h>overhum1)
+          {
+            alertmessage = "Humidity to High";  
+          }
+          else
+          {
+            alertmessage = "Humidity to Low";  
+          }
+      
+      }
+  }
+ Serial.println(alertmessage);
   http.addHeader("Content-Type", "application/json");
-  //int httpResponseCode = http.POST("{\"Temperature\":\""  + tempstring + "\",\"Humidity\":\""+ h +"\", \"Username\":\""+username+"\",\"Password\":\""+password+"\"}");
-  t=dht.readTemperature();
-  h=dht.readHumidity();
+  http.addHeader("Authorization","Basic " + base64code);
+  int httpResponseCode = http.POST("{\"Alertmessage\":\""  + alertmessage+"\"}");
+
+    Serial.println(httpResponseCode);
+    Serial.println("Sended");
+    
+  http.end();
+  Serial.println("Over");
+  Serial.println(over);
+  Serial.println(preferences.getString("SimulateOverHeat","false"));
+  if(over==1 || preferences.getString("SimulateOverHeat","false")=="True")
+  {
+    if(preferences.getString("SimulateOverHeat","false")=="True")
+    {
+      alertmessage = "Simulated overheat alert";
+      preferences.putString("SimulateOverHeat","false");
+    }
+    sendalert(alertmessage,base64code);
+
+    
+      http.end();
+  }
   server.handleClient();
   MDNS.update();
+  over=0; 
 }
+
+
+
+
 void setAcces()
 {
    username = preferences.getString("username","User");
    password = preferences.getString("password","User");
-  Serial.println("====");
-  Serial.println(username);
-
+   Serial.println("====");
+   Serial.println(username);
+   String postvalue = username+":" +password;
+   String base64code = base64::encode(postvalue);
     Serial.println("1");
-    HTTPClient http; //Object of class HTTPClient
+    HTTPClient http;
 
 
-    http.beginRequest();
-    http.get(request->url);
-    http.sendHeader("x-rapidapi-key:" + request->apiKey);;
-    http.endRequest();
-    
-    http.addHeader("Content-Type", "application/json");
+
+
     http.begin(client,"http://192.168.137.80:245/device/register");
-     http.POST("{}");
+    DynamicJsonDocument jsonBuffer(1024);
+    http.addHeader("Content-Type", "application/json");
+    http.POST("{\"Key\":\"value\"}");
       
-      DynamicJsonDocument jsonBuffer(1024);
       deserializeJson(jsonBuffer,http.getString());
       Serial.println(http.getString());
       JsonObject root = jsonBuffer.as<JsonObject>();
@@ -115,20 +194,52 @@ void setAcces()
        Serial.print("To syn j");
        Serial.println(root);
        password = String(root["password"]); 
-    
+
     Serial.println(preferences.getString("password","User"));
     Serial.println("haslo");
     Serial.println(username);
     
-    
     http.end(); //Close connection
-  
   
   preferences.putString("username",username);
   preferences.putString("password",password);
   Serial.println("||||||");
   Serial.println(preferences.getString("username","User"));
   }
+  
+  void getconfiguration(){
+    HTTPClient http;
+
+     username = preferences.getString("username","User");
+     password = preferences.getString("password","User");
+     Serial.println(username);
+     String postvalue = username+":" +password;
+     String base64code = base64::encode(postvalue);
+
+    http.begin(client,"http://192.168.137.80:245/config");
+    http.addHeader("Authorization","Basic " + base64code);
+     DynamicJsonDocument configuration(1024);
+     int code = http.GET();
+     if(code=200)
+     {
+      deserializeJson(configuration,http.getString());
+      Serial.println(http.getString());
+      JsonObject root2 = configuration.as<JsonObject>();
+       String mintemp = String(root2["minTemp"]); 
+       Serial.print("To syn j2");
+       Serial.println(root2);
+       preferences.putString("maxTemp",String(root2["maxTemp"])); 
+       preferences.putString("minTemp", String(root2["frequency"]));
+       preferences.putString("minHumidity",String(root2["minHumidity"]));
+       preferences.putString("maxHumidity",String(root2["maxHumidity"]));
+       
+      
+      }
+      else{
+        basiconfig();
+        }
+      http.end();
+    }
 void toggleLED()
 {
   digitalWrite(pin_led,!digitalRead(pin_led));
@@ -136,3 +247,41 @@ void toggleLED()
   
   server.send(204,"");
 }
+
+
+void sendalert(String alertmessage, String base64code){
+    HTTPClient http;
+      String alertserv = serverName + "/alert"; 
+      http.begin(client,alertserv);
+      http.addHeader("Content-Type", "application/json");
+      http.addHeader("Authorization","Basic " + base64code);
+      int httpResponseCode = http.POST("\""+alertmessage+"\"");
+
+    Serial.println(httpResponseCode);
+    Serial.println("Sended2");
+    http.end();
+  
+  }
+
+  
+void sim(){
+  preferences.putString("SimulateOverHeat","True");
+  Serial.println("Overheat");
+  Serial.println(preferences.getString("SimulateOverHeat","false"));
+  }
+
+  
+void clearr(){
+
+    Serial.println("Cleared");
+  } 
+
+  
+void basiconfig(){
+      preferences.putInt("maxTemp",100);
+    preferences.putInt("minTemp",1);
+    preferences.putInt("maxHumidity",100);
+    preferences.putInt("minHumidity",1);
+  
+  }
+ 

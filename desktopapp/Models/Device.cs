d15b19d6f39060d7e6c4d6a8492f733bbe78ff65
Piotr <stasiososnass@gmail.com>
2023-11-10 08:16:59 +0100

    user and device

---
+++ desktopapp/Models/Device.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktopapp.Models
+{
+    public class Device
+    {
+        public string devicelog { get; set; }
+        public double temp { get; set; }
+        public double humidity { get; set; }
+
+        public Device(string devicelog) {
+        this.devicelog = devicelog;
+
+        
+        
+        }
+        public async void getdevice() { 
+        await Task.Delay(1000);
+           
+        }
+
+
+
+
+    }
+}
---
+++ desktopapp/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktopapp.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public int acces { get; set; }
+        public User(string username, string password)
+        { 
+        this.username = username;
+        this.password = password;
+   
+        }
+        public async Task<int> LogIn() { 
+        int result = await getacces();
+            return result;
+        
+        }
+        public async Task<int> getacces() {
+            await Task.Delay(1000);
+            return 0;
+        
+        }
+
+
+
+    }
+}
